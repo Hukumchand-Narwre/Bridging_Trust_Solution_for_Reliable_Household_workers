b9ee528938e048cac3f2ae35151c3bed41af23bf
Akanksha Adase <akankshaadase1307@gmail.com>
2023-10-02 18:09:45 +0530

    user card

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import classes from "./UserCard.module.css";
 const UserCard = () => {
   return (
-    <div className={classes.flex}>
-      <div>card</div>
-      <div>card</div>
-      <div>card</div>
+    <div className="m-20">
+      <div className={classes.flex}>
+        <div className={`bg-white/50  ${classes.card1}`}>
+          <div className={classes.img}>
+            <img src="/Logo.jpeg" alt="Logo" className={classes.border} />
+          </div>
+          <h2>Hukumchand Narwre</h2>
+          <a href="#" className={classes.mybutton}>
+            View Details
+          </a>
+        </div>
+        <div className={`bg-white/50  ${classes.card1}`}>
+          <div className={classes.img}>
+            <img src="/Logo.jpeg" alt="Logo" className={classes.border} />
+          </div>
+          <h2>Akanksha Adase</h2>
+          <a href="#" className={classes.mybutton}>
+            View Details
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
