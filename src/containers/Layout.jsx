26a469f68960dcb9de526d8aeeca8e68a083c479
Hukumchand-Narwre <hukumchandnarwre@gmail.com>
2023-10-29 20:12:10 +0530

    homescreen

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header/Header";
 const Layout = ({ children }) => {
   return (
     <div>
-      <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
+      {/* <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"></div> */}
       <Header />
       <div>{children}</div>
     </div>
