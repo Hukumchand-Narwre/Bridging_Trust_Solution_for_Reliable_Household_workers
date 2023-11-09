386da4ba7f9cdc2024c55df623ecc57544403296
Hukumchand Narwre <85044429+Hukumchand-Narwre@users.noreply.github.com>
2023-11-09 19:57:38 +0530

    Dev (#19)
    * test
    
    * Starting admin card screen
    
    * layout-route-bg-done
    
    * complete login screen
    
    * user card
    
    * complete login
    
    * worker create account
    
    * incomplete admin screen
    
    * complete create account for user worker
    
    * ok
    
    * complete user
    
    * image-fix
    
    * admin-firstscreen
    
    * homescreen
    
    * fix
    
    * fix
    
    * image support for worker
    
    * fix: image upload
    
    * image-classifier
    
    * fix
    
    ---------
    
    Co-authored-by: Achal Jichkar <achalj.cse20@sbjit.edu.in>
    Co-authored-by: Achal Jichkar <93928078+Achalj14@users.noreply.github.com>
    Co-authored-by: Akanksha Adase <akankshaadase1307@gmail.com>
    Co-authored-by: akanksha1307 <89465549+akanksha1307@users.noreply.github.com>
    Co-authored-by: mayur ramdham <mayurramdham145@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import ROUTES, { RenderRoutes } from "./routes/Routes";
+
 import "./App.css";
+import { useAuthStore } from "./store/Auth";
+import ImageRecognition from "./components/Machine Learning Model/ImageRecognition";
+import ImageClassifier from "./screen/AIModel/ImageClassifier";
+import ImageSimilarityChecker from "./screen/AIModel/ImageSimilarityChecker";
+
 function App() {
-  return (
-    <>
-      <h1>Major project started repository set up done</h1>
-    </>
-  );
+  const { token } = useAuthStore();
+  return <RenderRoutes routes={ROUTES} token={token} />;
+  // return <ImageRecognition />;
+  // return <ImageClassifier />;
+  //return <ImageSimilarityChecker />;
 }
 
 export default App;
